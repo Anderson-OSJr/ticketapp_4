1a293c2cc7172a3738ff715cb3b8317f462ee0e6
ANDERSON DE OLIVEIRA E SILVA JUNIOR <87553205+Anderson-OSJr@users.noreply.github.com>
2024-07-06 12:13:01 -0300

    After Date/Time setup && Status & Priority components creation

---
+++ app/tickets/DataTable.tsx
@@ -0,0 +1,78 @@
+import TicketPriority from "@/components/TicketPriority";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Ticket } from "@prisma/client";
+
+interface Props {
+  tickets: Ticket[];
+}
+
+const DataTable = ({ tickets }: Props) => {
+  /* console.log("Era para ser a rela do tickets"); */
+  return (
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>
+                <div className="flex justify-center">Title</div>
+              </TableHead>
+              <TableHead>
+                <div className="flex justify-center">Status</div>
+              </TableHead>
+              <TableHead>
+                <div className="flex justify-center">Priority</div>
+              </TableHead>
+              <TableHead>
+                <div className="flex justify-center">Created At</div>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id} data-href="/">
+                    <TableCell>
+                      <div className="flex justify-center">{ticket.title}</div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketPriority priority={ticket.priority} />
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        {ticket.createdAt.toLocaleDateString("pt-BR", {
+                          day: "2-digit",
+                          month: "2-digit",
+                          year: "2-digit",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: true,
+                        })}
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default DataTable;
---
+++ components/TicketPriority.tsx
@@ -0,0 +1,32 @@
+import { Priority } from "@prisma/client";
+import { Flame } from "lucide-react";
+
+interface Props {
+  priority: Priority;
+}
+
+const priorityMap: Record<Priority, { label: string; level: 1 | 2 | 3 }> = {
+  LOW: { label: "Low", level: 1 },
+  MEDIUM: { label: "Medium", level: 2 },
+  HIGH: { label: "High", level: 3 },
+};
+
+const TicketPriority = ({ priority }: Props) => {
+  return (
+    <>
+      <Flame className="text-red-500" />
+      <Flame
+        className={`${
+          priorityMap[priority].level > 1 ? "text-red-500" : "text-gray-700"
+        }`}
+      />
+      <Flame
+        className={`${
+          priorityMap[priority].level > 2 ? "text-red-500" : "text-gray-700"
+        }`}
+      />
+    </>
+  );
+};
+
+export default TicketPriority;
