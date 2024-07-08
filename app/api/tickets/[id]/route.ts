fee3a0caf9e9147f2383cf7abd2d3be08d30f198
ANDERSON DE OLIVEIRA E SILVA JUNIOR <87553205+Anderson-OSJr@users.noreply.github.com>
2024-07-08 11:56:56 -0300

    After create api/ticket/[id]/route.ts PATCH endpoint

---
+++ app/api/tickets/[id]/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/prisma/db";
+import { ticketSchemaZodValidator } from "@/ValidationSchemas/ticket";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  const validation = ticketSchemaZodValidator.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const ticketToBeUpdated = await prisma.ticket.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!ticketToBeUpdated) {
+    return NextResponse.json({ error: "Ticket Not Found" }, { status: 404 });
+  }
+
+  const updatedTicket = await prisma.ticket.update({
+    where: { id: ticketToBeUpdated.id },
+    data: { ...body },
+  });
+
+  return NextResponse.json(updatedTicket, { status: 201 });
+}
